4f274531310ce91043e763aa7a7435f799337bbc
Slawosz Slawinski <slawosz@gmail.com>
2023-09-26 11:22:16 +0100

    Allow to use any image as database docker image in kubernetes deployment

--- terraform/application/database.tf
+++ terraform/application/database.tf
@@ -12,6 +12,8 @@ module "postgres" {
   azure_enable_monitoring     = var.enable_monitoring
   azure_enable_backup_storage = var.enable_postgres_backup_storage
   server_version              = "14"
+  server_docker_image         = "postgis/postgis:14-3.4"
+  azure_extensions            = ["POSTGIS"]
 }
 
 
