f08cd5bbf490823c963999205b0023c2fd0cc766
Slawosz Slawinski <slawosz@gmail.com>
2023-10-17 14:21:35 +0100

    Fix CI

--- db/migrate/20230626101457_enable_postgis.rb
+++ db/migrate/20230626101457_enable_postgis.rb
@@ -1,6 +1,8 @@
 class EnablePostgis < ActiveRecord::Migration[7.0]
   def change
+    # rubocop:disable Rails/ReversibleMigration
     # enable_extension 'postgis'
     execute "CREATE EXTENSION IF NOT EXISTS postgis"
+    # rubocop:enable Rails/ReversibleMigration
   end
 end
