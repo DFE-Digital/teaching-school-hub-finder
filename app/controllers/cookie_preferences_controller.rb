a559c334960e161cc3318d40e7d6ff61dfe62026
Slawosz Slawinski <slawosz@gmail.com>
2024-08-16 13:20:05 +0100

    Cookies banner with javascript

--- app/controllers/cookie_preferences_controller.rb
+++ app/controllers/cookie_preferences_controller.rb
@@ -7,29 +7,32 @@ class CookiePreferencesController < ApplicationController
         value: cookie_preferences_params[:consent],
         expires: 1.year.from_now,
       }
-      cookies["hide-cookies-banner"] = {
-        value: "0",
-        expires: 1.year.from_now,
-      }
 
-      redirect_back(fallback_location: "/")
+      respond_to do |format|
+        format.json do
+          render json: {
+            status: "ok",
+            message:,
+          }
+        end
+
+        format.html do
+          flash[:success] = "You’ve set your cookie preferences."
+          redirect_to "/pages/cookies"
+        end
+      end
     else
       render :new, status: :unprocessable_entity
     end
-  end
-
-  def hide
-    cookies["hide-cookies-banner"] = {
-      value: "1",
-      expires: 1.year.from_now,
-    }
-
-    redirect_back(fallback_location: "/")
   end
 
 private
 
   def cookie_preferences_params
     params.require(:cookie_preferences).permit(:consent)
+  end
+
+  def message
+    %(You’ve #{cookie_preferences_params[:consent] == 'accept' ? 'accepted' : 'rejected'} analytics cookies.)
   end
 end
--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,5 @@
 import { initAll } from "govuk-frontend";
 
+import "./cookie-banner"
+
 initAll();
