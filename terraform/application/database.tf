de7534625445885ca943152b5186a512ac125312
paul senior <paul.senior@education.gov.uk>
2025-10-13 09:15:23 +0100

    feat(2128): manage postgis in tf module - testing

--- terraform/application/database.tf
+++ terraform/application/database.tf
@@ -12,7 +12,7 @@ module "postgres" {
   azure_enable_monitoring     = var.enable_monitoring
   azure_enable_backup_storage = var.enable_postgres_backup_storage
   server_version              = "14"
-  server_docker_image         = "postgis/postgis:14-3.4"
+  server_postgis_version      = "14-3.4"
   azure_extensions            = ["POSTGIS"]
   azure_sku_name              = var.postgres_flexible_server_sku
 }
